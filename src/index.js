9bdffd68faad5d9c1935889f4d10e8e88022f0c8
Akshar Parmar <akshar.parmar@somaiya.edu>
2023-06-09 14:43:35 +0530

    Added flights model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const {PORT} = require('./config/serverConfig.js');
 const ApiRoutes = require('./routes/index');
-const {Airplane} = require('./models/index.js');
+//const {Airplane} = require('./models/index.js');
 const SetupAndStartServer = async()=>{
     
     //create the express object
@@ -10,10 +10,6 @@ const SetupAndStartServer = async()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api',ApiRoutes);
-
-    // await Airplane.create({
-    //         modelNumber:'Bombardier CRJ'
-    // })
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`); //string interpolation
     })
